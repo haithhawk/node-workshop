eb552c7e2911a599071bb62d956034da6581a7ec
Haitham <haithhawk@gmail.com>
2017-11-19 16:08:12 +0200

    finished step3

--- src/server.js
+++ src/server.js
@@ -1,6 +1,16 @@
 var http = require('http');
-var server = http.createServer();
+
+var message = 'I am so happy to be partof the workshop'
+function handler(request , response){
+  var method = request.method;
+  console.log(method);
+var endpoint = request.url;
+console.log(endpoint);
+response.writeHead(200 , {"Content-Type":"text/html"});
+response.write(message);
+response.end();
+}
+var server = http.createServer(handler);
 server.listen(3000 , function(){
 console.log("server is listening on port 3000 . ready to accept requests!");
-
 } );
